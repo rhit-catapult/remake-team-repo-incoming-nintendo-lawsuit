95626ae698318c8bdc2b3845754b69c82bc25272
RHIT-KupskiZM <zachkupski@gmail.com>
2025-06-07 13:04:59 -0400

    jumping

--- character.py
+++ character.py
@@ -11,10 +11,10 @@ class Player:
         self.screen = screen
         self.x = x
         self.y = y
-        self.gravity = .02
+        self.gravity = .002
         self.velocity_y = 0
         self.velocity_x = 0
-        self.on_ground = False
+        self.on_ground = True
         self.hitbox = (self.x,self.y,self.x+46,self.y+50)
     def draw(self,screen):
         screen.blit(self.image, (self.x, self.y))
@@ -23,24 +23,46 @@ class Player:
         self.x += self.velocity_x
         self.y += self.velocity_y
 
-
-
 def test():
     pygame.init()
     resolution = (1000,600)
     screen = pygame.display.set_mode(resolution)
     fps = pygame.time.Clock()
     fps.tick(60)
-    player = Player(screen,10,10)
+    player = Player(screen,500,300)
+    player_speed = .5
+    player_jump_power = .5
+    jump_timer = 0
+    jump_debounce = False
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    player.velocity_x += player_speed
+                if event.key == pygame.K_LEFT:
+                    player.velocity_x += -player_speed
+                if event.key == pygame.K_UP and player.on_ground:
+                    player.velocity_y = -player_jump_power
+                    player.on_ground = False
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    player.velocity_x -= player_speed
+                if event.key == pygame.K_LEFT:
+                    player.velocity_x += player_speed
+
         screen.fill((255,255,255))
         if player.on_ground != True:
-            player.velocity_y += player.gravity
-            if player.velocity_y > 0.3:
-                player.velocity_y = 0.3
+            if jump_debounce == False:
+                jump_timer = 0
+                jump_debounce = True
+            else:
+                jump_timer += 1
+            if jump_timer > 45:
+                player.velocity_y += player.gravity
+                if player.velocity_y > 0.3:
+                    player.velocity_y = 0.3
         player.draw(screen)
         player.move()
         pygame.display.update()
