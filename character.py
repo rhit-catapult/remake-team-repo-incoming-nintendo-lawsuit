3cc9ce7d0f2681ce6b8517a536aebd2d68fda971
RHIT-KupskiZM <zachkupski@gmail.com>
2025-06-07 12:44:35 -0400

    fallingsprite

--- character.py
+++ character.py
@@ -3,6 +3,11 @@ import sys
 
 class Player:
     def __init__(self, screen,x,y):
+        self.imagetemp = pygame.image.load("SCH_Module.png")
+        self.imagewidth = self.imagetemp.get_rect().width
+        self.imageheight = self.imagetemp.get_rect().height
+        self.image = pygame.transform.scale(self.imagetemp, (self.imagewidth / 7.5, self.imageheight / 7.5))
+        self.rect = self.image.get_rect()
         self.screen = screen
         self.x = x
         self.y = y
@@ -10,8 +15,10 @@ class Player:
         self.velocity_y = 0
         self.velocity_x = 0
         self.on_ground = False
-    def draw(self):
-        pygame.draw.rect(self.screen, (0,0,255), (self.x, self.y, 30, 50))
+        self.hitbox = (self.x,self.y,self.x+46,self.y+50)
+    def draw(self,screen):
+        screen.blit(self.image, (self.x, self.y))
+       # pygame.draw.rect(self.screen, (0,0,255), (self.x, self.y, 30, 50))
     def move(self):
         self.x += self.velocity_x
         self.y += self.velocity_y
@@ -34,7 +41,7 @@ def test():
             player.velocity_y += player.gravity
             if player.velocity_y > 0.3:
                 player.velocity_y = 0.3
-        player.draw()
+        player.draw(screen)
         player.move()
         pygame.display.update()
 test()
