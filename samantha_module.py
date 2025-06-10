df56727f62ede8b87bf16fd3370c393371bd43f3
sammy <atlas.nights777@gmail.com>
2025-06-09 22:05:04 -0400

    The Sprites

--- samantha_module.py
+++ samantha_module.py
@@ -1,18 +1,36 @@
-import pygame.transform
-from pygame.examples.scrap_clipboard import screen
-from pygame.examples.sprite_texture import running
+import pygame
+import sys
+import time
+import random
 
-BG = pygame.transform.scale(BG, (200, 200))
+class Clouds:
+    def __init__(self, screen, x, image_filename):
+        self.screen = screen
+        self.x = x
+        self.image =pygame.image.load(image_filename)
 
-x = 7400
-y = 1000
+    def draw(self):
+        self.screen.blit(self.image, (self.x, 2725))
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame QUIT:
-            running = False
-    screen.fill((0,0,0))
-    screen.blit(BG, x, y))
-    pygame.display.update()
-pygame.quit()
-    print("End -->")
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((1000, 600))
+    pygame.display.set_caption("samantha_module")
+    cloud = Clouds(screen, 0, "Clouds.png")
+    clock = pygame.time.Clock()
+    while True:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    pressed_keys = pygame.key.get_pressed()
+    if pressed_keys[pygame.K_LEFT]:
+        cloud.x -= 10
+    if pressed_keys[pygame.K_RIGHT]:
+        cloud.x += 10
+
+if __name__ == "__main__":
+    main()
